bb70143b9ecaea0bc58421ac7feeaa543ec6349a
Attila Domokos <adomokos@gmail.com>
2018-02-21 09:47:08 +0100

    Verify Client exists on sharded Asset

--- lib/stinger/sharded/asset.rb
+++ lib/stinger/sharded/asset.rb
@@ -2,6 +2,13 @@ module Stinger
   module Sharded
     class Asset < Sharded::Base
       has_many :vulnerabilities
+
+      # belongs_to :client won't work,
+      # as the Client record is in a
+      # different DB
+      def client
+        Stinger::Client.find(client_id)
+      end
     end
   end
 end
--- spec/sharded/asset_spec.rb
+++ spec/sharded/asset_spec.rb
@@ -2,31 +2,35 @@ require 'spec_helper'
 
 RSpec.describe Stinger::Sharded::Asset do
   before do
-    # ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
 
-  subject(:asset) do
-    Octopus.using(:client_3) do
-      FactoryBot.create(:asset,
-                        :ip_address_locator,
-                        :client_id => SHARDED_CLIENT_ID)
-    end
-  end
-
-  it 'has many vulnerabilities' do
     cve = FactoryBot.create(:cve, :apple)
+    FactoryBot.create(:client, :sharded)
     Octopus.using(:client_3) do
+      asset = FactoryBot.create(:asset,
+                                :ip_address_locator,
+                                :client_id => SHARDED_CLIENT_ID)
       FactoryBot.create(:vulnerability,
                         :client_id => SHARDED_CLIENT_ID,
                         :asset => asset,
-                        :cve => cve)
+                        :cve_id => cve.id)
     end
+  end
 
+  subject(:asset) do
+    Stinger::Sharded::Asset.using(:client_3).first
+  end
+
+  it 'has many vulnerabilities' do
     asset_count =
       Stinger::Sharded::Asset
       .using(:client_3)
       .count
 
     expect(asset_count).to eq(1)
+  end
+
+  it "can find it's client" do
+    expect(asset.client).not_to be_nil
   end
 end

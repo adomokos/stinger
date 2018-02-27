245f42d7816c017e26936eb364aeded35dbf3774
Attila Domokos <adomokos@gmail.com>
2018-02-27 04:58:46 -0600

    Simplify Asset bulk ETL

--- tasks/asset_bulk_etl.rb
+++ tasks/asset_bulk_etl.rb
@@ -14,15 +14,7 @@ Stinger::Asset.where(:client_id => 2).take(20).each do |a|
   sa.save!
   Stinger::Sharded::Vulnerability.bulk_insert do |worker|
     a.vulnerabilities.each do |v|
-      worker.add(
-        :id => v.id,
-        :client_id => v.client_id,
-        :asset_id => v.asset_id,
-        :cve_id => v.cve_id,
-        :notes => v.notes,
-        :created_at => v.created_at,
-        :updated_at => v.updated_at
-      )
+      worker.add(v.attributes)
     end
   end
   counter += 1
